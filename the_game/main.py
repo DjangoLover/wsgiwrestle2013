96df486d929cda000cce18ee4855baba093a3a71
Wayne Werner <wayne@waynewerner.com>
2013-09-28 08:10:08 -0500

    Added some things

--- the_game/main.py
+++ the_game/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from the_game import games as the_games
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +8,12 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
+
+
+@app.route("/games", defaults={'game': None})
+@app.route("/games/<game>")
+def games(game):
+    return render_template('game_list.html', games=the_games.games)
 
 
 def start():
