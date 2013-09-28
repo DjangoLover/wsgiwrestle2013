25964aa1f98b4bab2f8bcd38fbb63126424867bb
Wayne Werner <wayne@waynewerner.com>
2013-09-27 20:56:52 -0500

    Basic server & script (the-game) setup

--- the_game/main.py
+++ the_game/main.py
@@ -1,2 +1,14 @@
+import os
+import logging
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
 def start():
-    print("hi!")
+    port = int(os.environ.get('PORT', 6662))
+    app.run('0.0.0.0', debug=True, port=port)
